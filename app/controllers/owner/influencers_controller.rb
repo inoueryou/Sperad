9eeff9e172b8032f6b369043a031f0367c4a7efb
vagrant <vagrant@localhost.localdomain>
2020-10-12 17:36:39 +0000

    owner/influencer/top

--- app/controllers/owner/influencers_controller.rb
+++ app/controllers/owner/influencers_controller.rb
@@ -1,2 +1,15 @@
 class Owner::InfluencersController < ApplicationController
+
+	def index
+		@influencers = Influencer.where(is_valid: "Available")
+	end
+
+	def show
+		@infliencer = Influencer.find(params[:id])
+	end
+
+	private
+	def influencer_params
+		params.require(:influencer).permit(:name, :kana_name, :genre_id, :prefectures, :account, :email, :is_valid, :sns_follower, :profile_image, :enthusiasm)
+	end
 end
