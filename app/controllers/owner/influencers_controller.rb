3913f7270523a611b364a6e3ad71f2d280b8aa12
vagrant <vagrant@localhost.localdomain>
2020-10-13 08:19:26 +0000

    owner/influencers/genre-search

--- app/controllers/owner/influencers_controller.rb
+++ app/controllers/owner/influencers_controller.rb
@@ -1,7 +1,14 @@
 class Owner::InfluencersController < ApplicationController
+	before_action :authenticate_owner!
 
 	def index
+		@genres = Genre.all
+		if params["genre"]
+			@influencers = Influencer.where(genre_id: params["genre"])
+			@genre = Genre.find(params["genre"])
+		else
 		@influencers = Influencer.where(is_valid: "Available")
+		end
 	end
 
 	def show
