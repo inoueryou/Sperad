57f744c781eee8460d0d00bbddd6e91066a6f121
vagrant <vagrant@localhost.localdomain>
2020-10-28 12:41:35 +0000

    update

--- db/migrate/20201006170606_create_messages.rb
+++ db/migrate/20201006170606_create_messages.rb
@@ -1,9 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-      t.references :influencer_id, foreign_key: true
-      t.references :owner_id, foreign_key: true
-      t.references :room_id, foreign_key: true
+      t.integer :influencer_id, foreign_key: true
+      t.integer :owner_id, foreign_key: true
+      t.integer :room_id, foreign_key: true
       t.text :content
       t.timestamps
     end
--- db/migrate/20201015043952_create_rooms.rb
+++ db/migrate/20201015043952_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
-      t.references :influencer_id, foreign_key: true
-      t.references :owner_id, foreign_key: true
+      t.integer :influencer_id, foreign_key: true
+      t.integer :owner_id, foreign_key: true
       t.timestamps
     end
   end
